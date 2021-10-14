8b92f2a280fbb85f1707b9405d91112050abd615
ASJADSYED08 <hr49335@gmail.com>
2021-10-15 00:42:44 +0500

    first commit

---
+++ code.py
@@ -0,0 +1,10 @@
+print("Enter num 1")
+num1=input()
+print("Enter num 2")
+num2=input()
+try:
+    print("Sum of num1 & num2 is=",int(num1)+int(num2))
+except Exception as e:
+    print(e)
+
+print("This line is very important")
